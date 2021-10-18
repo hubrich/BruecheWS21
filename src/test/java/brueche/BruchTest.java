a1c608c99a365a96ed0ae255ba1cac94720e0e96
Tibornator <s571106@htw-berlin.de>
2021-10-18 10:29:07 +0200

    Commit aus dem Terminal

--- src/test/java/brueche/BruchTest.java
+++ src/test/java/brueche/BruchTest.java
@@ -1,2 +1,24 @@
-package brueche;public class BruchTest {
+package brueche;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BruchTest {
+
+    @Test
+    @DisplayName("dies ist der erste Test, mir fällt jetzt keine sinnvolle Beschreibung ein")
+    void testAusrechnen(){
+
+        Bruch bruch = new Bruch(5, 2);
+
+        var actual = bruch.ausrechnen() ;
+        var expected = 2.5;
+
+        assertEquals(expected, actual);
+
+    }
+
+
 }
